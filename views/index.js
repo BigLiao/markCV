deca2dcf2a5722a1bd9580bcbd0ac001c7c35745
liao <mail@liaozhibin.com>
2019-10-22 00:05:24 +0800

    打包发布功能

--- views/index.js
+++ views/index.js
@@ -1,8 +1,1 @@
 import './style.scss';
-const markdownIt = require('markdown-it');
-const resume = require('../markdown/resume.md');
-
-const mdt = markdownIt();
-const result = mdt.render(resume.default);
-
-document.getElementById('markdown-body').innerHTML = result;
