b9d24b85fcd47bbb0feaee3b2524f195969102cb
liao <mail@liaozhibin.com>
2019-10-22 00:29:54 +0800

    功能开发完成==

---
+++ bin/cli.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const pkg = require('../package.json');
+const program = require('commander');
+
+program
+  .version(pkg.version)
+  .usage('<markdown_file>')
+  .action((value, cmd) => {
+    require('../lib/build/cli')(value, cmd);
+  });
+
+program
+  .command('build')
+  .description('Generate resume site')
+  .action((value, cmd) => {
+    require('../lib/build/cli')(value, cmd);
+  });
+
+program
+  .command('publish')
+  .description('Publish website')
+  .action((value, cmd) => {
+    require('../lib/publish/cli')(value, cmd);
+  });
+
+program
+  .command('serve')
+  .description('Run web server locally')
+  .action((value, cmd) => {
+    require('../lib/serve/cli')(value, cmd);
+  });
+
+
+
+program.parse(process.argv);
